0b51a93eeaa5f49b0393f2274edb8fd61157fa96
Kelly Lifchez <klifchez3@gatech.edu>
2025-02-10 20:13:38 -0500

    figure edit

--- homework4/programs/hw4_fishbycatch.py
+++ homework4/programs/hw4_fishbycatch.py
@@ -56,13 +56,13 @@ ax = plt.gca()
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 
-month_labels = ['Jan 2017', 'Feb 2017', 'Mar 2017', 'Apr 2017', 'May 2017', 'Jun 2017', 'Jul 2017', 'Aug 2017', 'Sep 2017', 'Oct 2017', 'Nov 2017', 'Dec 2017',
-                'Jan 2018', 'Feb 2018', 'Mar 2018', 'Apr 2018', 'May 2018', 'Jun 2018', 'Jul 2018', 'Aug 2018', 'Sep 2018', 'Oct 2018', 'Nov 2018', 'Dec 2018']
+month_labels = ['Jan 2017', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+                'Jan 2018', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 plt.xticks(ticks=range(1, 25), labels=month_labels, rotation=90)
 
 plt.text(12.5, plt.ylim()[1], 'Program Start', horizontalalignment='center', verticalalignment='bottom', color='black')
 
-plt.savefig(os.path.join(outputpath, 'trend_plot.pdf'))
+plt.savefig(os.path.join(outputpath, 'trend_plot.pdf'), bbox_inches='tight', pad_inches=0.1)
 
 plt.show()
 
